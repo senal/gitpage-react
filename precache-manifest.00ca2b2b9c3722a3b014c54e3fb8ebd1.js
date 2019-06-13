3efdac52d4578e5367dbd1f4c90696bdb09abbb1
Ranga Fernando <ranga.fernando@gmail.com>
2019-06-13 21:59:56 +1000

    Updates

---
+++ precache-manifest.00ca2b2b9c3722a3b014c54e3fb8ebd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28eb1c8b2af0dcfff13c6afc9475ceae",
+    "url": "/gitpage-react/index.html"
+  },
+  {
+    "revision": "c8f31111a5d68809626e",
+    "url": "/gitpage-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gitpage-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c8f31111a5d68809626e",
+    "url": "/gitpage-react/static/js/main.1443ef43.chunk.js"
+  },
+  {
+    "revision": "2cb790787da978fdf41e",
+    "url": "/gitpage-react/static/js/runtime~main.63333727.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitpage-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
